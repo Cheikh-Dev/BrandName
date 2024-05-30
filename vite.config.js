2e9155b56ebb14c769ffb8af9885ca13fba745a0
Cheikh-Ahmed-Tidiane-Gueye <cheikhahmedtidiane219@gmail.com>
2024-05-30 09:12:49 +0000

    Add homepage sscript to package.json

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Cheikh-Dev/",
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: "[name]-[hash].[ext]",
-        chunkFileNames: "[name]-[hash].js",
-        entryFileNames: "[name]-[hash].js",
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       assetFileNames: "[name]-[hash].[ext]",
+  //       chunkFileNames: "[name]-[hash].js",
+  //       entryFileNames: "[name]-[hash].js",
+  //     },
+  //   },
+  // },
 });
