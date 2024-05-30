1265e75b3f5f24a3d4ef570af734acd500880e03
Cheikh-Ahmed-Tidiane-Gueye <cheikhahmedtidiane219@gmail.com>
2024-05-30 09:50:06 +0000

    r

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Cheikh-Dev/",
-  // build: {
-  //   rollupOptions: {
-  //     output: {
-  //       assetFileNames: "[name]-[hash].[ext]",
-  //       chunkFileNames: "[name]-[hash].js",
-  //       entryFileNames: "[name]-[hash].js",
-  //     },
-  //   },
-  // },
 });
